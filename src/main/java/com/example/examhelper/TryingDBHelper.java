6fa82105c40c7eec52397efd8465b76b17f8b2e3
ctrlc0ctrlv <46068723+ctrlc0ctrlv@users.noreply.github.com>
2019-02-01 14:08:22 +0500

    Добавляем отображение таблиц (в том числе и для базы)

--- src/main/java/com/example/examhelper/TryingDBHelper.java
+++ src/main/java/com/example/examhelper/TryingDBHelper.java
@@ -18,7 +18,7 @@ public class TryingDBHelper extends SQLiteOpenHelper {
         private final Context fContext;
 
         TryingDBHelper(Context context) {
-            super(context, DB_NAME, null, 14);
+            super(context, DB_NAME, null, 17);
             this.fContext = context;
         }
         public void createDataBase() throws IOException {
