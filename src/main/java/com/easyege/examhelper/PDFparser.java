25fe7ac012117bc0bd6f3411eaf488e8832cbc22
ctrlc0ctrlv <46068723+ctrlc0ctrlv@users.noreply.github.com>
2019-05-13 18:53:02 +0500

    Подключение парсера pdf-файлов Ознакомление с содержимым библиотеки PDFbox

--- src/main/java/com/easyege/examhelper/PDFparser.java
+++ src/main/java/com/easyege/examhelper/PDFparser.java
@@ -11,53 +11,48 @@ import java.io.File;
 public class PDFparser {
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static void main(String[] args) throws Exception {
-        /*String string = "I";
-        System.out.println("after compress:");
-        byte [] compressed = compress(string);
-        System.out.println(Arrays.toString(compressed));
-        System.out.println("after decompress:");
-        String decomp = decompress(compressed);
-        System.out.println(decomp);
-        System.out.println("just byte array:");
-        System.out.println(Arrays.toString(string.getBytes()));
-        System.out.println("one more byte array:");
-        System.out.println(Arrays.toString(string1.getBytes(StandardCharsets.UTF_8)));*/
-
         PDDocument doc = PDDocument.load(new File("C:\\Users\\Рома\\Desktop\\215.pdf"));
         PDFTextStripper stripper = new PDFTextStripper();
+
         String text = stripper.getText(doc);
         System.out.println(text);
         doc.close();
+
+
+        /* Запись текста и прикрепление картинки к документу pdf
+
+        PDDocument document = PDDocument.load(new File("C:\\Users\\Рома\\Desktop\\216.pdf"));
+        PDPage page = document.getPage(0);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        //Begin the Content stream
+        contentStream.beginText();
+        //Setting the font to the Content stream
+        contentStream.setFont(PDType1Font.TIMES_ROMAN, 16);
+        //Setting the position for the line
+        contentStream.setLeading(16f);
+        //Setting the position for the line
+        contentStream.newLineAtOffset(25, 725);
+
+        String text1 = "This is an example of adding text to a page in the pdf document. we can add as many lines";
+        String text2 = "as we want like this using the ShowText()  method of the ContentStream class";
+
+        //Adding text in the form of string
+        contentStream.showText(text1);
+        contentStream.newLine();
+        contentStream.showText(text2);
+        //Ending the content stream
+        contentStream.endText();
+
+        PDImageXObject pdImage = PDImageXObject.createFromFile("C:\\Users\\Рома\\Desktop\\tick.png", document);
+        pdImage.setHeight(300);
+        pdImage.setWidth(300);
+        contentStream.drawImage(pdImage, 90, 400);
+        contentStream.close();
+
+        //Saving the document
+        document.save("C:\\Users\\Рома\\Desktop\\216.pdf");
+        //Closing the document
+        document.close();
+        */
     }
-
-
-    /*@RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private static byte[] compress(String str) throws Exception {
-        if (str == null || str.length() == 0) {
-            return new byte[0];
-        }
-        //System.out.println("String length : " + str.length());
-        ByteArrayOutputStream obj=new ByteArrayOutputStream();
-        GZIPOutputStream gzip = new GZIPOutputStream(obj);
-        gzip.write(str.getBytes(StandardCharsets.UTF_8));
-        gzip.close();
-        //String outStr = obj.toString("UTF-8");
-        //System.out.println("Output String length : " + outStr.length());
-        return obj.toByteArray();
-    }*/
-
-    /*@RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private static String decompress(byte [] str) throws Exception {
-
-        //System.out.println("Input String length : " + str.length);
-        GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(str));
-        BufferedReader bf = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
-        StringBuilder outStr = new StringBuilder();
-        String line;
-        while ((line=bf.readLine())!=null) {
-            outStr.append(line);
-        }
-        //System.out.println("Output String lenght : " + outStr.length());
-        return outStr.toString();
-    }*/
 }
